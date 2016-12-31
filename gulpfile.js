7151f3ed0f4279c83e3b707e953c675afec8079a
Ken Guie <nyhunter77@yahoo.com>
2016-12-31 15:26:21 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// Gulp.js configuration
+
+// include gulp and plugins
+var gulp = require('gulp');
+
+// file locations
+var
+  source = 'source/',
+  dest = 'build';
+
+//default task
+gulp.task('default', function() {
+
+});
